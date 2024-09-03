36b82e7ab6bac80ee041d62c90c26e0bbb8276cc
Philippe Grosjean <phgrosjean@sciviews.org>
2024-09-03 19:27:55 +0200

    Correction of txo bugs: (1) package:utils not found and (2) out of bound in RStudio in resolve_lang() when LANGUAGE environment variable is not set

--- R/zzzz.R
+++ R/zzzz.R
@@ -1,4 +1,11 @@
 .onLoad <- function(libname, pkgname){
+  # We need to explicitly set language, otherwise there is a problem in RStudio:
+  # error in strsplit(target_language, ":")[[1]][[1]] : subscript out of bound
+  # when running resolve_lang()
+  if (is.na(Sys.getenv("LANGUAGE", NA)))
+    Sys.setLanguage("en")
+
+  library(utils) # Apparently required for R CMD check despite Depends: utils ?!
   utils <- asNamespace('utils')
 
   package_env <- as.environment('package:utils')
